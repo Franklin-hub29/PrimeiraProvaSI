a86f0938a78d318317cf5dab1db55be1ece655ee
Franklin-hub29 <81197526+Franklin-hub29@users.noreply.github.com>
2021-04-12 21:52:34 -0300

    Teste

---
+++ src/Testegithub/TesteDeEnvioParaGuit.java
@@ -0,0 +1,32 @@
+package Testegithub;
+
+public class TesteDeEnvioParaGuit {
+	
+	public static void main(String[]args) {
+		 
+		float a = 10f , b = 10f ,resultado ;
+		
+		resultado = a % b ;
+		
+		if(resultado == 0) {
+			System.out.println("Divisão de x por Y e perfeita");
+		}else {
+			System.out.println("Divisão de x por Y Resta "+ resultado);
+		}
+		
+		
+	}
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
